813fdae5b92f6ef9f4825a1a578321a4afd5732e
Kaori Kubo <kaori@KaorinoMacBook-Air.local>
2025-02-20 18:06:31 +0900

    feat:新規作成機能_SaveData

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Todo;
 
-// 下記必要？消す？
 use Illuminate\Http\Request;
+
 
 class TodoController extends Controller
 {
@@ -22,4 +22,16 @@ class TodoController extends Controller
   
     return view('todo.create'); 
 }
+
+public function store(Request $request) 
+{
+    $content = $request->input('content');
+    $todo = new Todo();
+    $todo->content = $content;
+    $todo->save();
+
+    return redirect()->route('todo.index');
 }
+}
+
+
