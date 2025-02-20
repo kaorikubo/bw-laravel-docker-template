baaf91f8ea2805f315d55cc2c869bbf33a6fe5a2
Kaori Kubo <kaori@KaorinoMacBook-Air.local>
2025-02-20 15:18:27 +0900

    feat:todo一覧_getData

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
+// 下記必要？消す？
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();
+
         return view('todo.index');
     }
 }
